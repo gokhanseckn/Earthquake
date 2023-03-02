603879ae5f42383fcd52ec267062054e9ca647f3
gokhanseckn <gokhanseckn@gmail.com>
2023-03-02 09:40:55 +0300

    bottom sheet view styling

--- Earthquake/View/EarthquakeBottomSheetView.swift
+++ Earthquake/View/EarthquakeBottomSheetView.swift
@@ -12,18 +12,54 @@ struct EarthquakeBottomSheetView: View {
     var earthquake: Earthquake
     
     var body: some View {
-        VStack {
+        ZStack {
             MapView(cities:[City(
                 name: earthquake.province ?? "",
                 coordinate: CLLocationCoordinate2D(
                     latitude: Double(earthquake.latitude)!,
                     longitude: Double(earthquake.longitude)!))]
             )
-            .frame(height: 400)
-            .padding(.top, 20)
+            .ignoresSafeArea()
             
-            Text(earthquake.location)
-            Spacer()
+            VStack {
+                Spacer()
+                HStack {
+                    Text(String(format: "%.1f", Double(earthquake.magnitude) ?? "0"))
+                        .font(.headline)
+                        .padding()
+                        .background(earthquake.color)
+                        .cornerRadius(10)
+                    
+                    VStack(alignment: .leading) {
+                        Text(earthquake.province ?? earthquake.location)
+                            .font(.headline)
+                        Text(earthquake.district ?? "")
+                            .foregroundColor(.black.opacity(0.6))
+                    }
+                    
+                    Spacer()
+                    
+                    VStack(spacing: 2) {
+                        HStack {
+                            Text("\(earthquake.depth) km")
+                            Text("•")
+                            Text("\(Earthquake.dateFormatter.string(from: earthquake.formattedDate))")
+                        }
+                        Text(Earthquake.relativeDateTimeFormatter.localizedString(for: earthquake.formattedDate, relativeTo: Date.now))
+                    }
+                    .font(.footnote)
+                    .fontWeight(.regular)
+                    .foregroundColor(.secondary)
+                }
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(.ultraThickMaterial)
+                )
+                .padding()
+            }
+            
+            
         }
     }
 }
